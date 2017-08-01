484f382dd3897a661b6c063c2b3a5198b8916731
playpool513 <playpool513@gmail.com>
2017-08-01 17:20:52 +1000

    Add spec: place method assigns value to x, y, f only if all args is valid

--- spec/lib/toy_robot_spec.rb
+++ spec/lib/toy_robot_spec.rb
@@ -33,4 +33,79 @@ describe ToyRobot do
   it { should respond_to(:x) }
   it { should respond_to(:y) }
   it { should respond_to(:f) }
+
+  let(:toy_robot) { ToyRobot.new }
+  let(:x_units) { 5 }
+  let(:y_units) { 5 }
+  let(:directions) { %w(NORTH EAST SOUTH WEST) }
+
+  describe '#place' do
+    context 'x, y, f are all valid' do
+      let(:x) { rand(x_units) }
+      let(:y) { rand(y_units) }
+      let(:f) { directions.sample }
+
+      before { toy_robot.place(x, y, f) }
+
+      it 'assigns toy_robot.x as x' do
+        expect(toy_robot.x).to eq(x)
+      end
+      it 'assigns toy_robot.y as y' do
+        expect(toy_robot.y).to eq(y)
+      end
+      it 'assigns toy_robot.f as f' do
+        expect(toy_robot.f).to eq(f)
+      end
+    end
+
+    context 'any of x, y, f is invalid' do
+      context 'x is invalid' do
+        let(:x) { -2 }
+        let(:y) { rand(y_units) }
+        let(:f) { directions.sample }
+
+        it 'does not change toy_robot.x' do
+          expect { toy_robot.place(x, y, f) }.to_not change { toy_robot.x }
+        end
+        it 'does not change toy_robot.y' do
+          expect { toy_robot.place(x, y, f) }.to_not change { toy_robot.y }
+        end
+        it 'does not change toy_robot.f' do
+          expect { toy_robot.place(x, y, f) }.to_not change { toy_robot.f }
+        end
+      end
+
+      context 'y is invalid' do
+        let(:x) { rand(x_units) }
+        let(:y) { 12 }
+        let(:f) { directions.sample }
+
+        it 'does not change toy_robot.x' do
+          expect { toy_robot.place(x, y, f) }.to_not change { toy_robot.x }
+        end
+        it 'does not change toy_robot.y' do
+          expect { toy_robot.place(x, y, f) }.to_not change { toy_robot.y }
+        end
+        it 'does not change toy_robot.f' do
+          expect { toy_robot.place(x, y, f) }.to_not change { toy_robot.f }
+        end
+      end
+
+      context 'f is invalid' do
+        let(:x) { rand(x_units) }
+        let(:y) { rand(y_units) }
+        let(:f) { "SOUTHWEST" }
+
+        it 'does not change toy_robot.x' do
+          expect { toy_robot.place(x, y, f) }.to_not change { toy_robot.x }
+        end
+        it 'does not change toy_robot.y' do
+          expect { toy_robot.place(x, y, f) }.to_not change { toy_robot.y }
+        end
+        it 'does not change toy_robot.f' do
+          expect { toy_robot.place(x, y, f) }.to_not change { toy_robot.f }
+        end
+      end
+    end
+  end
 end
