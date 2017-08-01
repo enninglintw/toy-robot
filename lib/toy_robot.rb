d7fe62005c44f130cf84083f59186667e0156342
playpool513 <playpool513@gmail.com>
2017-08-02 03:43:59 +1000

    Implement ToyRobot.read(file)

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -1,6 +1,13 @@
 class ToyRobot
   def self.read(file)
     text = File.open(file).read
-    text.split("\n")
+    text.split("\n").map do |command|
+      case command
+      when /(^MOVE$|^LEFT$|^RIGHT$|^REPORT$)/i
+        $1
+      when /(PLACE)\s*(\d*),\s?(\d*),\s?(NORTH$|EAST$|SOUTH$|WEST$)/i
+        [$1, $2.to_i, $3.to_i, $4]
+      end
+    end.compact
   end
 end
